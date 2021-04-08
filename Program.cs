525c67734d5a53c9efefacae6c06510660f36913
nima-kam <nimakambarani007@gmail.com>
2021-04-08 22:22:16 +0430

    Transitions dictionary and Add method added.

--- Program.cs
+++ Program.cs
@@ -12,19 +12,34 @@ namespace FA
         public string name;
         public List<State> sources = new List<State>();
         public List<string> S_alphabet = new List<string>();
-        public List<State> destinations = new List<State>();
-        public List<string> D_alphabet = new List<string>();
+        Dictionary<string, List<State>> DTransitions;
+     //   public List<State> destinations = new List<State>();
+     //   public List<string> D_alphabet = new List<string>();
 
         public State(string name)
         {
             this.name = name;
         }
+        
+        /// <summary>
+        /// for adding a transition from this state to others.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="destination"></param>
+        public void AddTransition(string key,State destination)
+        {
+
+        }
     }
-   class DFA
+    class NFA
+    {
+         
+    }
+    class DFA
     {
         public List<State> dfa = new List<State>();
 
-        public DFA(List <State> dfa)
+        public DFA(List<State> dfa)
         {
             this.dfa = dfa;
         }
@@ -37,7 +52,7 @@ namespace FA
             List<State> FA = new List<State>();
             Console.WriteLine("Please, Enter the states like ( q0,q1,q2, ...):");
             string[] Statenames = Console.ReadLine().Split(',');
-            for(int i =0; i< Statenames.Length; i++)
+            for (int i = 0; i < Statenames.Length; i++)
             {
                 State temp = new State(Statenames[i]);
                 FA.Add(temp);
@@ -48,17 +63,17 @@ namespace FA
             string[] FinalStates = Console.ReadLine().Split(',');
             Console.WriteLine("Please, Enter how many rules you want to enter: ");
             int n = int.Parse(Console.ReadLine());
-            for(int i=0; i< n; i++)
+            for (int i = 0; i < n; i++)
             {
                 string[] token = Console.ReadLine().Split(' ');
-                for(int j =0; j < FA.Count; j++)
+                for (int j = 0; j < FA.Count; j++)
                 {
                     // fill sources
-                    if(token[0] == FA[j].name)
+                    if (token[0] == FA[j].name)
                     {
-                        for(int t =0; t < FA.Count; t++)
+                        for (int t = 0; t < FA.Count; t++)
                         {
-                            if(token[1] == FA[t].name)
+                            if (token[1] == FA[t].name)
                             {
                                 FA[j].destinations.Add(FA[t]);
                                 FA[j].D_alphabet.Add(token[2]);
